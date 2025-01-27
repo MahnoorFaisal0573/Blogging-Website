f08ad4583210d925c21e29f91009f039b06b8b1e
l1f21bsse0573 <l1f21bsse0573@ucp.edu.pk>
2025-01-28 00:28:44 +0500

    Fix missing styles and scripts

---
+++ dist/index.447059e2.js
@@ -0,0 +1,2 @@
+console.log("Welcome to the Blogging Website!"),document.querySelector("h1").textContent="Welcome to My Blogging Website!";
+//# sourceMappingURL=index.447059e2.js.map
---
+++ src/main.js
@@ -0,0 +1,3 @@
+// src/index.js
+console.log("Welcome to the Blogging Website!");
+document.querySelector('h1').textContent = "Welcome to My Blogging Website!";
